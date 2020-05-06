8726ae2b39040f91d1fa5ab39aaeb7a736dbab5d
adityasunny1189 <adityapathak1189@gmail.com>
2020-05-07 02:26:04 +0530

    second commit solved the image bug

--- covid-tracker/src/components/card/card.component.jsx
+++ covid-tracker/src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.styles.css'
 
 const Card = (props) => (
     <div className="card-container">
-        <img style = {{height: 200, width: 200, margin: "auto"}} alt = "country" src = {`https://www.countryflags.io/${props.monster.iso2}/shiny/64.png`} />
+        <img style = {{height: 200, width: 200, margin: "auto"}} alt = "country" src = {`https://www.countryflags.io/${props.cont.iso2}/shiny/64.png`} />
         <h2>Country: {props.cont.countryRegion}</h2>
         <h3>Confirmed: {props.cont.confirmed}</h3> 
         <h3>Recovered: {props.cont.recovered}</h3> 
